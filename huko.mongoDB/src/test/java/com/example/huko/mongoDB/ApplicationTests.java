e0bd7d6545a657589e26b54ec2cb7fc51dd367b1
TejashreePanchal <trpanchal@myseneca.ca>
2024-08-07 09:48:10 -0400

    Updated application with environment variable configuration

--- huko.mongoDB/src/test/java/com/example/huko/mongoDB/ApplicationTests.java
+++ huko.mongoDB/src/test/java/com/example/huko/mongoDB/ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.huko.mongoDB;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Value("${DATABASE_URL}")
+	private String databaseUrl;
+
 	@Test
 	void contextLoads() {
+		System.out.println("DATABASE_URL: " + databaseUrl);
 	}
-
 }
