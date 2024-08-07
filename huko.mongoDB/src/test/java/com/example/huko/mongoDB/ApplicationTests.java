a6e387df46614d730163c48821be33dc2f03fd69
TejashreePanchal <trpanchal@myseneca.ca>
2024-08-07 11:41:38 -0400

    Updated Docker and Vercel configuration

--- huko.mongoDB/src/test/java/com/example/huko/mongoDB/ApplicationTests.java
+++ huko.mongoDB/src/test/java/com/example/huko/mongoDB/ApplicationTests.java
@@ -3,8 +3,10 @@ package com.example.huko.mongoDB;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"DATABASE_URL=mongodb+srv://Tejashree:Tejashree@cluster0.efbz1eq.mongodb.net/HUKO?retryWrites=true&w=majority&appName=Cluster0"})
 class ApplicationTests {
 
 	@Value("${DATABASE_URL}")
