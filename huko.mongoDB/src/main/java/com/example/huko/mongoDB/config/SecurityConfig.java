b9296118a81ce99300e20c8101f50086618392d8
TejashreePanchal <trpanchal@myseneca.ca>
2024-08-08 00:14:08 -0400

    Fix circular dependency and update Dockerfile

--- huko.mongoDB/src/main/java/com/example/huko/mongoDB/config/SecurityConfig.java
+++ huko.mongoDB/src/main/java/com/example/huko/mongoDB/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.huko.mongoDB.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
+@Lazy
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
